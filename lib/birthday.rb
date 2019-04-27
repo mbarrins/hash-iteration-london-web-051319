5612feea345a67e57957b025119da51452efd5d8
Maija Barrins <maija.barrins@gmail.com>
2019-04-27 11:35:54 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,7 +5,9 @@
 # }
 
 def happy_birthday(birthday_kids)
-  # add your code snippet here!
+  birthday_kids.each {|kid, age| puts "Happy Birthday #{kid}! You are now #{age} years old."}
 end
 
-
+def age_appropriate_birthday(birthday_kids)
+  birthday_kids.each {|kid, age| puts "Happy Birthday #{kid}! You are now #{age} years old." if age < 12}
+end
